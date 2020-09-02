6db8060eb2779097e8ddd2d452f13250acc7caf5
Nguyễn Thế Tình <37909676+NguyenTheTinh-HCMUS@users.noreply.github.com>
2020-09-02 16:24:00 +0700

    Add project files.

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async  Task<IActionResult> GetAll()
+        {
+            return Json(
+
+              new { data = await  _db.Book.ToListAsync() }
+            );
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var BookfromBb = await _db.Book.FindAsync(id);
+            if (BookfromBb == null)
+            {
+                return Json(new { 
+                success=false,
+                message="Error while Deleting"
+                });
+            }
+            _db.Book.Remove(BookfromBb);
+            await _db.SaveChangesAsync();
+            return Json(new
+            {
+                success = true,
+                message = "Delete successful"
+            });
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Book Book { get; set; }
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+        public async Task OnGet(int id)
+        {
+
+            this.Book =  await _db.Book.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var bookFromDb = await _db.Book.FindAsync(this.Book.Id);
+                bookFromDb.Name = this.Book.Name;
+                bookFromDb.Author = this.Book.Author;
+                bookFromDb.ISBN = this.Book.ISBN;
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+
+            }
+            else
+            {
+                return RedirectToPage();
+
+            }
+        }
+    }
+}
